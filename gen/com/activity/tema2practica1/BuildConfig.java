881d80b049d64719361871b8401be73e38296cb6
omar marco <omar_mt_91@hotmail.com>
2014-10-14 19:54:52 +0200

    Prueba

---
+++ gen/com/activity/tema2practica1/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.activity.tema2practica1;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/activity/tema2practica1/Principal.java
@@ -0,0 +1,15 @@
+package com.activity.tema2practica1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Principal extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_principal);
+	}
+}
